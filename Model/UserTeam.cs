0290dcf2d5113134a734136322c29dcc8cfca07f
awloc <awloc@EXPO>
2025-06-10 15:07:52 +0200

    Model UserTeam

---
+++ Model/UserTeam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esport_Team_Hub.Model
+{
+    internal class UserTeam
+    {
+        public int UserID { get; set; }
+        public int TeamID { get; set; }
+        public DateTime JoinedAt { get; set; }
+
+        public UserTeam ()
+        {
+            JoinedAt = DateTime.Now;
+        }
+
+        public UserTeam (int userID, int teamID, DateTime joinedAt)
+        {
+            UserID = userID;
+            TeamID = teamID;
+            JoinedAt = joinedAt;
+        }
+    }
+}
