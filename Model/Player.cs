859067126d30604ffabd0764b4bb849504712b2b
awloc <awloc@EXPO>
2025-06-10 13:41:10 +0200

    Model Player

---
+++ Model/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esport_Team_Hub.Model
+{
+    public enum Platform
+    {
+        PC,
+        PlayStation,
+        Xbox
+    }
+    internal class Player
+    {
+        public int Id { get; set; }
+        public Platform Platform { get; set; }
+        
+        public Player (int id, Platform platform)
+        {
+            Id = id;
+            Platform = platform;
+        }
+    }
+}
